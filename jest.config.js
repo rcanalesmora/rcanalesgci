151c8b6e30dda0ad48798ef9b63fa88b081006e3
Roberto Canales <rcanales@autentia.com>
2020-01-20 23:27:38 +0100

    cambiamos rootdir

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   roots: ["<rootDir>/src/test/"],
+  rootDir: ["<rootDir>/src/test/"],
+
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
