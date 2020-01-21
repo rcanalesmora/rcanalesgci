38a8dccd5949489f9d885e4c697196d5b43e0d30
Roberto Canales <rcanales@autentia.com>
2020-01-21 22:58:11 +0100

    arreglado problema

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export class Operaciones {
-  static falloVoluntario sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+  static sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
 }
 
 // let a = 10;
