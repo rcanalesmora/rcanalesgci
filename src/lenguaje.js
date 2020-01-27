7c710a27105e2e7a1d0ca00344ffe299bfc56563
Roberto Canales <rcanales@autentia.com>
2020-01-27 22:37:48 +0100

    funcionando en una clase que retorna la respuesta

--- src/lenguaje.js
+++ src/lenguaje.js
@@ -1,11 +1,25 @@
 var request = require("request");
-var options = {
-    method: "GET",
-    url: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-09-13&hd=true",
-    headers: {}
-};
-request(options, function (error, response) {
-    if (error)
-        throw new Error(error);
-    console.log(response.body);
-});
+var GestorNoticiasNASA = /** @class */ (function () {
+    function GestorNoticiasNASA(URL) {
+        this.respuesta = null;
+        this.URL = "";
+        this.URL = URL;
+    }
+    GestorNoticiasNASA.prototype.recupera = function () {
+        var options = {
+            method: "GET",
+            url: this.URL,
+            headers: {}
+        };
+        this.respuesta = request(options, function (error, response) {
+            if (error)
+                throw new Error(error);
+            // console.log(response.body);
+        });
+    };
+    return GestorNoticiasNASA;
+}());
+var gestorNASA = new GestorNoticiasNASA("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-09-13&hd=true");
+gestorNASA.recupera();
+console.log("-------------------------");
+console.log(gestorNASA.respuesta);
--- src/lenguaje.ts
+++ src/lenguaje.ts
@@ -1,12 +1,31 @@
-var request = require("request");
+let request = require("request");
 
-var options = {
-  method: "GET",
-  url:
-    "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-09-13&hd=true",
-  headers: {}
-};
-request(options, function(error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+class GestorNoticiasNASA {
+  respuesta: any = null;
+  URL: string = "";
+
+  constructor(URL: string) {
+    this.URL = URL;
+  }
+
+  recupera() {
+    let options = {
+      method: "GET",
+      url: this.URL,
+      headers: {}
+    };
+
+    this.respuesta = request(options, function(error: any, response: any) {
+      if (error) throw new Error(error);
+
+      // console.log(response.body);
+    });
+  }
+}
+
+let gestorNASA = new GestorNoticiasNASA(
+  "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-09-13&hd=true"
+);
+gestorNASA.recupera();
+console.log("-------------------------");
+console.log(gestorNASA.respuesta);
