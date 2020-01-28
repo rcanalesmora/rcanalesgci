27dcf6969879436971929a8e2e76200e98002148
Roberto Canales <rcanales@autentia.com>
2020-01-28 23:24:23 +0100

    Asigando un interfaz y JSON.parse

--- src/lenguaje.ts
+++ src/lenguaje.ts
@@ -1,31 +1,42 @@
 let request = require("request");
 
+interface INoticiaNasa {
+  copyright: string;
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+}
+
 class GestorNoticiasNASA {
-  respuesta: any = null;
+  peticion: any = null;
   URL: string = "";
 
   constructor(URL: string) {
     this.URL = URL;
   }
 
+  anima = (error: any, response: any) => {
+    if (error) {
+      throw new Error(error);
+    } else {
+      let objeto: INoticiaNasa = JSON.parse(response.body);
+      console.log(objeto.copyright);
+    }
+  };
+
   recupera() {
-    let options = {
-      method: "GET",
-      url: this.URL,
-      headers: {}
-    };
+    let options = { method: "GET", url: this.URL, headers: {} };
 
-    this.respuesta = request(options, function(error: any, response: any) {
-      if (error) throw new Error(error);
-
-      // console.log(response.body);
-    });
+    this.peticion = request(options, this.anima);
   }
 }
 
 let gestorNASA = new GestorNoticiasNASA(
   "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-09-13&hd=true"
 );
+
 gestorNASA.recupera();
-console.log("-------------------------");
-console.log(gestorNASA.respuesta);
